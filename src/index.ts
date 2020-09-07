cd896e9aacac0b60d9c6ffa2f2680ef388c08b57
Griko Nibras <git@griko.id>
2020-09-07 20:26:20 +0700

    Extract types to separate files

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export * from "./airtable";
+export * from "./constants";
 export * from "./error";
+export * from "./field-types";
 export * from "./types";
--- src/types.ts
+++ src/types.ts
@@ -1,44 +1,6 @@
-export namespace Field {
-  export type RecordLink = string[];
-  export type SingleLineText = string;
-  export type LongText = string;
-  export type Checkbox = boolean;
-  export type MultipleSelect<T extends string = string> = T[];
-  export type SingleSelect<T extends string = string> = T[];
-  export type Collaborator = {
-    id: string;
-    email: string;
-    name: string;
-  };
-  export type Collaborators = Collaborator[];
-  export type Attachment = {
-    id: string;
-    url: string;
-    filename: string;
-    size: number;
-    type: string;
-    thumbnails?: {
-      small: Thumbnail;
-      large: Thumbnail;
-      full: Thumbnail;
-    };
-  };
-  export type DateType = Date | string;
-  export type PhoneNumber = string;
-  export type Email = string;
-  export type URL = string;
-  export type Number = number;
-  export type Currency = number;
-  export type Percent = number;
-  export type Duration = number;
-  export type Rating = number;
-}
+import * as Field from "./field-types";
 
-export interface Thumbnail {
-  url: string;
-  width: number;
-  height: number;
-}
+import { Timezone, UserLocale } from "./constants";
 
 export interface AirtableOptions {
   apiKey?: string;
@@ -94,8 +56,8 @@ export interface SelectOptions<T extends string | number | symbol> {
   }[];
   view?: string;
   cellFormat?: "json" | "string";
-  timeZone?: string;
-  userLocale?: string;
+  timeZone?: Timezone;
+  userLocale?: UserLocale;
 }
 
 export interface SelectResult<T extends FieldSet> extends TableRecords<T> {
